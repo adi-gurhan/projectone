7cf841a0c5362d0596ea2e39a9edd6eb8220daed
Adi.Gurhan <adi.gurhan@outlook.com>
2024-03-12 08:25:38 -0500

    Update App.js

--- App.js
+++ App.js
@@ -1,10 +1,16 @@
 import { createAppContainer } from "react-navigation";
 import { createStackNavigator } from "react-navigation-stack";
 import HomeScreen from "./src/screens/HomeScreen";
+import ListScreen from "./src/screens/ListScreen";
+import ComponentsScreen "./src/screens/ComponentsScreen";
+import ImageScreen from "./src/screens/ImageScreen";
 
 const navigator = createStackNavigator(
   {
     Home: HomeScreen,
+    List: ListScreen,
+    Components: ComponentsScreen,
+    Image: ImageScreen,
   },
   {
     initialRouteName: "Home",
